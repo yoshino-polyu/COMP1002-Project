21b139d8b059072c2eee8f730e39731cec4bb400
Simon-Xu-COMP <21096101d@connect.polyu.hk>
2021-11-20 16:40:35 +0800

    main

--- src/main.py
+++ src/main.py
@@ -82,12 +82,39 @@
 
 # main()
 
-f = open("test.txt", "w+")
-for i in range(10):
-    f.write("This is line %d\r\n" % (i + 1))
-f.close()
+def main():
+    while True:
+        option = str(input("Choose C for using command line interface, choose G for useing graphic user interface"))
+        if option == 'G':
+            # GUI
+            return 0
+        elif option == 'C':
+            while True:
+                # read all info from txt, and store it into storage.
+                storage = dict()
+                who = str(input("Input u to enter the user login page. Input a to enter the administrator login page. Input b to go back to the last menu."))
+                if who == 'u':
+                    id = str(input("Please input your user id").split())
+                    password = str(input("Please input your password").split())
+                    # change password
+                    # 
+                if who == 'a':
+                    
+                    
+                    break
+                if who == 'b':
+                    break
+        else:
+            # try catch needed
+            return 0
+                
+# f = open("test.txt", "w+")
+# for i in range(10):
+#     f.write("This is line %d\r\n" % (i + 1))
+# f.close()
 
-q = open("test.txt", "a+")
-for i in range(5):
-    q.write("append line %d\r\n" % (i + 1 + 10))
-q.close()
+# q = open("test.txt", "a+")
+# for i in range(5):
+#     q.write("append line %d\r\n" % (i + 1 + 10))
+# q.close()
+
