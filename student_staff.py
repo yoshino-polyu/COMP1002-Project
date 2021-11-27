e98bc7f73f7364327034a394701c278ac742a69d
JIANG Yiyang <a@imjyy.com>
2021-11-27 21:51:32 +0800

    Update student_staff.py

--- student_staff.py
+++ student_staff.py
@@ -13,3 +13,36 @@ class Student:
     id, last 
     """
     
+    @property
+    def id(self):
+        return self.id
+
+    @property
+    def password(self):
+        return self.pswrd
+   
+    @property
+    def lastname(self):
+        return self.last_n
+
+    @property
+    def firstname(self):
+        return self.first_n
+
+    @property
+    def depat(self):
+        return self.dpt
+    
+    @property
+    def num_inj(self):
+        return self.num_inj
+    
+    @property
+    def inj_info(self):
+        return self.inj_info
+
+    @property
+    def is_stu(self):
+        return self.is_stu
+
+    
