17f35b883a3e29da48947e9c8b07bf1c98880ba8
Simon-Xu-COMP <21096101d@connect.polyu.hk>
2021-11-28 03:06:08 +0800

    CLI update

--- view.py
+++ view.py
@@ -40,34 +40,27 @@ class View:
         return a
 
     def entry_page(self):
-        page = """
-        Please input a number:
-        1. using command line interface
-        2. using graphic user interface
-        3. exit program"""
-        print(page)
-    
+        print("Please input a number:")
+        print("1. using command line interface")
+        print("2. using graphic user interface")
+        print("3. exit program")
+
     def command_line(self):
-        page = """
-        Please input a number:
-        1. user login
-        2. administrator login
-        3. register an account
-        4. go back to the last page
-        5. exit program
-        """
-        print(page)
+        print("Please input a number:")
+        print("1. user login")
+        print("2. administrator login")
+        print("3. register an account")
+        print("4. go back to the last page")
+        print("5. exit program")
 
     def user_page(self):
-        page = """
-        Please input a number:
-        1. change the password
-        2. see current vaccination record
-        3. update vaccination record
-        4. go back to the last page
-        5. exit program"""
-        print(page)
-
+        print("Please input a number:")
+        print("1. change the password")
+        print("2. see current vaccination record")
+        print("3. update vaccination record")
+        print("4. go back to the last page")
+        print("5. exit program")
+        
     def inv_info(self):
         print("invalid input, please try again!")
         
