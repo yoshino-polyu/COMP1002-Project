70f20f200342fcf60ee7d42765963d006c2250f0
Simon-Xu-COMP <21096101d@connect.polyu.hk>
2021-11-27 21:44:46 +0800

    ddd

--- student_staff.py
+++ student_staff.py
@@ -1,6 +1,6 @@
 class Student:
     
-    def __init__(self, id : str, last_n : str, first_n : str, dpt : str, num_inj : str, inj_info : str, is_stu : str) -> None:
+    def __init__(self, id : str, last_n : str, first_n : str, dpt : str, num_inj : str, inj_info : str, is_stu : str, pswrd : str) -> None:
         self.id = id
         self.last_n = last_n
         self.first_n = first_n
@@ -8,6 +8,7 @@ class Student:
         self.num_inj = num_inj
         self.inj_info = inj_info
         self.is_stu = is_stu
+        self.pswrd = pswrd
     """
     id, last 
     """
