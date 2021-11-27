c737af6345d99a42b8df13a2301b12ded079fac4
JIANG Yiyang <a@imjyy.com>
2021-11-27 23:18:25 +0800

    Update student_staff.py

--- student_staff.py
+++ student_staff.py
@@ -17,41 +17,87 @@ class User:
     def id(self):
         return self.id
 
+    @id.setter
+    def id(self, value):
+        self.id = value
+        
+
     @property
     def pswrd(self):
         return self.pswrd
+
+    @pswrd.setter
+    def pswrd(self, value):
+        self.pswrd = value
+
    
     @property
     def last_n(self):
         return self.last_n
 
+    @last_n.setter
+    def last_n(self, value):
+        self.last_n = value
+
+
     @property
     def first_n(self):
         return self.first_n
 
+    @first_n.setter
+    def first_n(self, value):
+        self.first_n = value
+
+
     @property
     def dpt(self):
         return self.dpt
+
+    @dpt.setter
+    def dpt(self, value):
+        self.dpt = value
+
     
     @property
     def num_inj(self):
         return self.num_inj
+
+    @num_inj.setter
+    def num_inj(self, value):
+        self.num_inj = value
     
+
     @property
     def inj_info(self):
         return self.inj_info
+
+    @inj_info.setter
+    def inj_info(self, value):
+        self.inj_info = value
+        
 
     @property
     def is_stu(self):
         return self.is_stu
 
+    @is_stu.setter
+    def is_stu(self, value):
+        self.is_stu = value
+
+
+    @property
     def full_n(self):
         return self.last_n + " " + self.first_n
+
+    @full_n.setter
+    def full_n(self, value):
+        self.last_n + " " + self.first_n = value
     """
     returns the full name of this student or staff
     """
+   
+
     
-    @id.setter
-    def id(self, value):
-        self.id = value
+
     
+        
